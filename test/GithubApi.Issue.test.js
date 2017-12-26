932b3a9260950d64f83a055a6c5c94558dc452f0
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2017-12-26 17:40:51 -0500

    Checking for repository

--- test/GithubApi.Issue.test.js
+++ test/GithubApi.Issue.test.js
@@ -1,5 +1,5 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 
 const urlBase = 'https://api.github.com';
 
@@ -18,5 +18,25 @@ describe('Getting user info', () => {
 
   it('user has public repositories', () => {
     expect(user.public_repos).to.be.above(0);
+  });
+
+  describe('Listing user repositories', () => {
+    let repository;
+
+    const repoName = 'workshop-api-testing-js';
+
+    before(() => {
+      const repoQuery = agent.get(user.repos_url)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          repository = response.body.find(repo => repo.name === repoName);
+        });
+
+      return repoQuery;
+    });
+
+    it(`the repository ${repoName} exist`, () => {
+      assert.exists(repository);
+    });
   });
 });
