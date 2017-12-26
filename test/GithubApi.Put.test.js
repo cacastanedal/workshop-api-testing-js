38b79fdd9cd7f71c858327f4e4c7ccb026852545
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2017-12-26 11:22:18 -0500

    Checking put idenpotency

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -1,5 +1,5 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
 const statusCode = require('http-status-codes');
 
 const urlBase = 'https://api.github.com';
@@ -12,6 +12,27 @@ describe('Trying to follow someone through github API', () => {
       .then((response) => {
         expect(response.status).to.equal(statusCode.NO_CONTENT);
         expect(response.body).to.be.empty;
+      });
+  });
+
+  it('Watch if following aperdomob user', () => {
+    agent.get(`${urlBase}/user/following`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        assert.exists(response.body.find(user => user.login === 'aperdomob'));
+      });
+  });
+
+  it('Checking put idenpotency', () => {
+    agent.put(`${urlBase}/user/following/${userTest}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((firstResponse) => {
+        agent.put(`${urlBase}/user/following/${userTest}`)
+          .auth('token', process.env.ACCESS_TOKEN)
+          .then((secondResponse) => {
+            expect(firstResponse.status).to.equal(secondResponse.status);
+            expect(firstResponse.body).to.eql(secondResponse.body);
+          });
       });
   });
 });
