3d241c3fa5f6308505e3ac2e3a38d3ccbbe4d5c8
Carlos Castaneda Largacha <ccastanedal@pslcol.com.co>
2018-01-03 12:22:14 -0500

    Put to /gists with isomorphic fetch

---
+++ test/GithubApi.Gist.Fetch.test.js
@@ -0,0 +1,101 @@
+const fetch = require('isomorphic-fetch');
+// const { expect, assert } = require('chai');
+const { expect } = require('chai');
+const statusCode = require('http-status-codes');
+
+const chaiSubset = require('chai-subset');
+const chai = require('chai');
+
+const urlBase = 'https://api.github.com';
+
+chai.use(chaiSubset);
+
+describe('Creating a gist through Isomorphic-fetch', () => {
+  let gist;
+  let responseStatus;
+  // let gistUrl;
+
+  const postParams = {
+    description: 'This is a description example for a gists',
+    public: true,
+    files: {
+      'file1.txt': {
+        content: 'String file contents'
+      }
+    }
+  };
+
+  before(() => {
+    const gistQuery = fetch(`${urlBase}/gists`, {
+      method: 'Post',
+      headers: {
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
+      },
+      body: JSON.stringify(postParams)
+    }).then((response) => {
+      responseStatus = response.status;
+      return response.json();
+    }).then((gistCreated) => {
+      gist = gistCreated;
+      // gistUrl = response.body.url;
+    });
+
+    return gistQuery;
+  });
+
+  it('gist should have description, content and be public', () => {
+    expect(responseStatus).to.equal(statusCode.CREATED);
+    expect(gist.files['file1.txt'].content).to.equal(postParams.files['file1.txt'].content);
+    expect(gist).to.containSubset({
+      description: postParams.description,
+      public: true
+    });
+  });
+
+  /*
+  describe('Consulting gist through hipermedia', () => {
+    let gistMirror;
+    before(() => {
+      const gistGetQuery = agent.get(gistUrl)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          gistMirror = response.body;
+        });
+      return gistGetQuery;
+    });
+
+    it('the gist consulted exist', () => {
+      assert.exists(gistMirror);
+    });
+
+    describe('Deleting gist', () => {
+      let gistDeleted;
+      let deleteResponse;
+
+      before(() => {
+        const gistDelQuery = agent.del(gistUrl)
+          .auth('token', process.env.ACCESS_TOKEN)
+          .then((response) => {
+            gistDeleted = response.body;
+            deleteResponse = response.status;
+          });
+        return gistDelQuery;
+      });
+      it('the gist should be errased', () => {
+        expect(deleteResponse).to.equal(statusCode.NO_CONTENT);
+        expect(gistDeleted).to.be.empty;
+      });
+
+      describe('Trying to get the deleted gist', () => {
+        it('gist not found', () => {
+          agent.get(gistUrl)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .catch((response) => {
+              expect(response.status).to.equal(statusCode.NOT_FOUND);
+            });
+        });
+      });
+    });
+  });
+  */
+});
